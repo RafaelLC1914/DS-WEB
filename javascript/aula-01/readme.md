e9049091beb3db7b36936be40ed964d47426d18e
RafaelLC1914 <rafaellopescuan@gmail.com>
2025-01-31 12:26:24 -0300

    aula-01

---
+++ javascript/aula-01/readme.md
@@ -0,0 +1,8 @@
+aula introdutoria sobre Lógica de Programação com Javascript
+Exemplo de alert()
+Exemplo de confirm()
+Exibindo a opção escolhida pelo usúario
+Exemplo de prompet()
+Exibindo o nome do usuário
+Tentativa de Adição
+verificando o tipo de variavel
---
+++ javascript/aula-01/script.js
@@ -0,0 +1,21 @@
+console.log("Ola mundo");
+
+//Exemplo de alert()
+alert("Ola Mundo 1");
+//Exemplo de confirm()
+var teste = confirm ("Ola Mundo 2");
+    //Exibindo a opção escolhida pelo usúario 
+    console.log(teste);
+//Exemplo de prompet()
+    var testePrompt = prompt ("Digite seu nome")
+    //Exibindo o nome do usuário 
+    console.log(testePrompt)
+    alert(testePrompt)
+
+//Tentativa de Adição 
+    var numero1 = Number.parseInt (prompt("Digite o primeiro número"))
+    var numero2 = Number.parseInt (prompt("Digite o segundo número")) 
+    //verificando o tipo de variavel
+    console.log(typeof(numero1))
+    alert(numero1 + numero2);
+    
